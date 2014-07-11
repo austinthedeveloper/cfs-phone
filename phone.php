40b331cf1c3dc4913ed05ca0abc5ec95beaeae4f
Austin Stewart <ausstewa@gmail.com>
2014-07-11 14:42:10 -0500

    Removed a test script

--- phone.php
+++ phone.php
@@ -19,7 +19,7 @@ class cfs_phone extends cfs_field
     function html($field)
     {
     ?>
-        <input type="text" name="<?php echo $field->input_name; ?>" class="phoneFormat <?php echo $field->input_class; ?>" value="<?php echo $field->value; ?> <?php echo dirname( __FILE__ ); ?>" />
+        <input type="text" name="<?php echo $field->input_name; ?>" class="phoneFormat <?php echo $field->input_class; ?>" value="<?php echo $field->value; ?>" />
     <?php
     }
 
